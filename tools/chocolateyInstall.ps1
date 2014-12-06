8a1d74a338a636cfc742256ec3286ea260c95309
switchspan <taylor.kenneth@gmail.com>
2014-12-06 16:41:54 -0500

    Update nuspec and installer file

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageName = 'hugo' # arbitrary name for the package, used in messages
 $installerType = 'exe' #only one of these: exe, msi, msu
 $url = 'https://github.com/spf13/hugo/releases/download/v0.12/hugo_0.12_windows_386.zip' # download url
 $url64 = 'https://github.com/spf13/hugo/releases/download/v0.12/hugo_0.12_windows_amd64.zip' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/s' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 # main helpers - these have error handling tucked into them already
