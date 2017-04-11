424abb0e4775f8a300a4a83683a239a4467d1a93
Ken Taylor <ktaylor@gotechark.com>
2017-04-11 13:07:45 -0400

    update for v0.20 release of hugo

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Chocolatey install for Hugo
 - Edit the `./tools/chocolateyInstall.ps1` install script
 - You **must** save your files with *UTF-8* character encoding without BOM.
 
+_Note:_ Make sure you install checksum (`choco install checksum`) and calculate checksums for the 32bit and 64bit copies of hugo in the [Install-ChocolateyZipPackage](https://chocolatey.org/docs/helpers-install-chocolatey-zip-package) command of the `chocolateyInstall.ps1` file.
+
 ## Build the package
 
 - Run `choco pack` in the same directory as `hugo.nuspec`
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-Install-ChocolateyZipPackage -packageName 'hugo' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/spf13/hugo/releases/download/v0.19/hugo_0.19_Windows-32bit.zip' -checksum 'FA2660049E9DF0A75C8EC03CEBCB4D4A696579E1F111635BE7355549DBA011A0' -checksumType 'sha256'-Url64 'https://github.com/spf13/hugo/releases/download/v0.19/hugo_0.19_Windows-64bit.zip' -checksum64 '36DB6333346025C688F18F525C720C9B1BB4D9E5329E4A48F4BC507B033EE9B7' -checksumType64 'sha256'
+Install-ChocolateyZipPackage -packageName 'hugo' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/spf13/hugo/releases/download/v0.20/hugo_0.20_Windows-32bit.zip' -checksum 'B5AAAE719F01D997C63839077A8F19F599DC491277D2DECB6B5F4AA7C4887FAA' -checksumType 'sha256'-Url64 'https://github.com/spf13/hugo/releases/download/v0.20/hugo_0.20_Windows-64bit.zip' -checksum64 '0D74FFE4722BAA81626142FBFA75CBA75867F005E7F29EEEF227196E5FB5D2E4' -checksumType64 'sha256'
 
 $hugoExe = Get-ChildItem $(Split-Path -Parent $MyInvocation.MyCommand.Definition) | Where-Object -Property Name -Match "hugo_[\d\.]+_windows_(386|amd64)\.exe"
 
