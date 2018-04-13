0256c207423454f6944ed0e2dd94a05ce0fdf484
Ken Taylor <ktaylor@gotechark.com>
2018-04-12 22:28:15 -0400

    update to 0.38.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -11,9 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  checksum      = '640c76cf155eefb4d383d02077cde0b9e5bfc9d6db436fd2f7eeebe69fa50671'
+  checksum      = 'ebb2073927a3fb377da4c2f50af6638ce9f56ff83101d3c455e80b8953dee4d6'
   checksumType  = 'sha256'
-  checksum64    = '9dfda1f11a2a237cbc8b2bca5cf55cc9be9182d3644e2518e5a8af3c11492fc7'
+  checksum64    = 'b6d6db48d1d7a72352e685805339df5f8e76ef24a1f9c19f50d75ee7a227a01e'
   checksumType64= 'sha256'
 }
 
